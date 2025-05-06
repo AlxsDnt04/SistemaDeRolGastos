cac05086235d5d4112635e574e303a2feff2e2c9
AlxsD <carapedoxd@gmail.com>
2025-05-06 11:54:44 -0500

    Add total ingresos and egresos calculations; update form to include total fields

--- javascript/main.js
+++ javascript/main.js
@@ -16,8 +16,21 @@ function calcularIngreso() {
     const total_hora_100 = (hora_normal * hora_100 * 2).toFixed(2);
     document.getElementById("temp_total_100").value = total_hora_100; // Mostrar el total al 100% en el campo correspondiente
     //calculo total (sueldo + bonos + hora25 + hora50 + hora100)
-    const total = (sueldo + parseFloat(total_hora_25) + parseFloat(total_hora_50) + parseFloat(total_hora_100)).toFixed(2);
+    const total_ingresos = (sueldo + parseFloat(total_hora_25) + parseFloat(total_hora_50) + parseFloat(total_hora_100)).toFixed(2);
+    document.getElementById("total_ingresos").value = total_ingresos; // Mostrar el total de ingresos en el campo correspondiente
     
+}
+/* calcular egresos */
+function calcularEgresos() {
+    const iess = parseFloat(document.getElementById("iess").value);
+    const multas = parseFloat(document.getElementById("multas").value);
+    const atrasos = parseFloat(document.getElementById("atrasos").value);
+    const alimentacion = parseFloat(document.getElementById("alimentacion").value);
+    const anticipo = parseFloat(document.getElementById("anticipo").value);
+    const otros = parseFloat(document.getElementById("otros").value);
+    //calculo total (iess + multas + atrasos + alimentacion + anticipo + otros)
+    const total_egresos = (iess + multas + atrasos + alimentacion + anticipo + otros).toFixed(2);
+    document.getElementById("total_egresos").value = total_egresos; // Mostrar el total de egresos en el campo correspondiente
 }
 
 /* accion del boton submit */
@@ -32,11 +45,21 @@ formulario.addEventListener("submit", (e) => {
 
     // Obtener los valores de los campos de texto
     const campos = {
+        /* ingresos */
         total_25: document.getElementById("temp_total_25").value,
         total_50: document.getElementById("temp_total_50").value,
         total_100: document.getElementById("temp_total_100").value,
-        
-
+        /* total ing */
+        total_ingresos: document.getElementById("total_ingresos").value,
+        /* egresos */
+        iess: document.getElementById("iess").value,
+        multas: document.getElementById("multas").value,
+        atrasos: document.getElementById("atrasos").value,
+        alimentacion: document.getElementById("alimentacion").value,
+        anticipo: document.getElementById("anticipo").value,
+        otros: document.getElementById("otros").value,
+        /* total eng */
+        total_egresos: document.getElementById("total_egresos").value, 
     }
 
     for (const key in campos) {
